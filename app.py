f3b58eaedeedfbea238b14c48feb6d6683960d94
TigerSyam <N210193@rguktn.ac.in>
2025-08-01 02:32:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,6 @@ def get_transcript():
         return jsonify({'transcript': text})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    app.run(host='0.0.0.0', port=port)
