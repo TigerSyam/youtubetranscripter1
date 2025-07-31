b0259213a2c3a476d52997e44034c834e7e7bc57
TigerSyam <N210193@rguktn.ac.in>
2025-08-01 02:24:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask, request, jsonify
+from youtube_transcript_api import YouTubeTranscriptApi
 
+app = Flask(__name__)
+
+@app.route('/transcript', methods=['GET'])
+def get_transcript():
+    vid = request.args.get('id')
+    if not vid:
+        return jsonify({'error': 'Missing video ID'}), 400
+    try:
+        ytt_api = YouTubeTranscriptApi()
+        data =ytt_api.fetch(vid,preserve_formatting=True)
+        text = " ".join([e['text'] for e in data])
+        return jsonify({'transcript': text})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
